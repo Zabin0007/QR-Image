1a68b531f92c20f20729cad0a82bc7fadca46c38
Muhammed Zabin M <zabinmuhammed0000@gmail.com>
2025-02-06 11:42:50 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import image from '../public/chair.jpg'
+function App() {
+
+  return (
+    <>
+      <center>
+        <h3>Find This...!</h3>
+        <img src={image} width={'300px'} height={'auto'} alt="" />
+      </center>
+    </>
+  )
+}
+
+export default App
