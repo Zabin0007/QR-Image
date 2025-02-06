d751f7978bc05d1b46c982879822bd3a6cbdfd3a
Muhammed Zabin M <zabinmuhammed0000@gmail.com>
2025-02-06 11:50:59 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
+import { useState } from 'react'
 import image from '../public/chair.jpg'
 function App() {
+
+  const[result,setResult]=useState(6)
 
   return (
     <>
       <center>
         <h3>Find This...!</h3>
         <img src={image} width={'300px'} height={'auto'} alt="" />
+        <br />
+        <h2>{result}</h2>
       </center>
     </>
   )
